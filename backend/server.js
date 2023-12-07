83b52aca4d28e62913a3762011a18a7e88ef586d
Yuvraj Rekhi <yuvrajrekhi@wireless-10-104-60-59.umd.edu>
2023-12-06 23:49:55 -0500

    installed cors on backend, trying to fix api issue

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
   console.log("Time:", Date.now);
   next();
 });
+app.use(cors());
 app.use((req, res, next) => {
   {
     res.header('Access-Control-Allow-Origin', "*");
