3617be90a0cb3a308599ed6d391e7ee6a8036aa1
Yuvraj Rekhi <yuvrajrekhi@wireless-10-104-60-171.umd.edu>
2023-12-07 16:28:43 -0500

    change to use prod mailtrap smtp

--- backend/controllers/sendEmail.js
+++ backend/controllers/sendEmail.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 const nodemailer = require("nodemailer")
 
 const sendEmail = async (emailTo, subject, message) => {
-    var transporter = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: process.env.MAILTRAP_USERNAME,
-        pass: process.env.MAILTRAP_PASSWORD
-      }
-    });
+  var transporter = nodemailer.createTransport({
+    host: "live.smtp.mailtrap.io",
+    port: 587,
+    auth: {
+      user: process.env.MAILTRAP_USERNAME,
+      pass: process.env.MAILTRAP_PASSWORD
+    }
+  });
 
       let info = await transporter.sendMail({
         from: '"Secret Santa"', 
