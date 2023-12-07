2dc1d341d0b8b126c7db81b21aa8daa34fb40ed5
Yuvraj Rekhi <yuvrajrekhi@wireless-10-104-60-171.umd.edu>
2023-12-07 17:28:41 -0500

    minor mailtrap fixes

--- backend/controllers/sendEmail.js
+++ backend/controllers/sendEmail.js
@@ -14,7 +14,7 @@ const sendEmail = async (emailTo, subject, message) => {
     });
 
     const info = await transporter.sendMail({
-      from: '"Secret Santa" <santa.generator2023@live.smtp.mailtrap.io>',
+      from: '"Secret Santa" <santa.generator2023@gmail.com>',
       to: emailTo,
       subject: subject,
       text: message,
