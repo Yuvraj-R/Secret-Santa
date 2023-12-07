fe8ec1bfda4df61a37551b582e928e1144c57764
Yuvraj Rekhi <yuvrajrekhi@wireless-10-104-60-171.umd.edu>
2023-12-07 17:01:25 -0500

    updated mailtrap info

--- backend/controllers/sendEmail.js
+++ backend/controllers/sendEmail.js
@@ -1,38 +1,55 @@
 require("dotenv").config();
 
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (emailTo, subject, message) => {
-  var transporter = nodemailer.createTransport({
-    host: "live.smtp.mailtrap.io",
-    port: 587,
-    auth: {
-      user: process.env.MAILTRAP_USERNAME,
-      pass: process.env.MAILTRAP_PASSWORD
-    }
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "live.smtp.mailtrap.io",
+      port: 587,
+      auth: {
+        user: process.env.MAILTRAP_USERNAME,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    });
 
-      let info = await transporter.sendMail({
-        from: '"Secret Santa"', 
-        to: emailTo,
-        subject: subject, 
-        text: message,
-      });
+    const info = await transporter.sendMail({
+      from: '"Secret Santa" <santa.generator2023@gmail.com>',
+      to: emailTo,
+      subject: subject,
+      text: message,
+    });
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify(
-          {
-            message: 'Your function executed successfully!',
-            data: {
-                messageId: info.messageId,
-                previewURL: nodemailer.getTestMessageUrl(info)
-            },
+    console.log('Message sent: %s', info.messageId);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Your function executed successfully!',
+          data: {
+            messageId: info.messageId,
+            previewURL: nodemailer.getTestMessageUrl(info),
           },
-          null,
-          2
-        ),
-      };
-}
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          error: 'Internal Server Error',
+          message: 'An error occurred while sending the email.',
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
 
 module.exports = sendEmail;
